b6294f16791ccff005adcf8b95afe2804059b918
GuoChao <34301656+guoguobuchao@users.noreply.github.com>
2017-12-23 15:16:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ from multiprocessing import Pool
 import time,operate
 from get_info import get_latest_build
 if __name__ == '__main__':
-    t_ver = 'FCS8.2.X.0'
-    bld = get_latest_build(t_ver)
-    #bld = 62657
+    #edit the target_ver or edit build_number
+    target_ver = 'FCS8.2.X.0'
+    build_number = get_latest_build(target_ver)
+    
+    #build_number = 62657
     if not bld:
         print "build not found,please check t_ver;"
     else:
         print 'target build:'+str(bld)
     part = 0
-    devices =['10.65.255.115','10.65.255.116','10.65.255.114','10.65.5.225']
-    devs =['10.65.5.227','10.65.255.118']
+    
+    devs =['10.10.10.10','20.20.20.20']
     begin =time.time()
 
     pool = Pool()
-    for dev in devices:
+    for dev in devs:
         print dev
-        pool.apply_async(operate.upgrade,args=(dev,bld,str(part)))
+        pool.apply_async(operate.upgrade,args=(dev,build_number,str(part)))
 
     pool.close()
     pool.join()
